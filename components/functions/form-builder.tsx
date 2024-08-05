8bbdb51e59d6cddec48a49d90ceee42329ef1910
SyeddHassan <syeddhassanali@gmail.com>
2024-08-05 05:51:08 -0700

    progess

--- components/functions/form-builder.tsx
+++ components/functions/form-builder.tsx
@@ -2,34 +2,39 @@
 
 import React from "react";
 import { Form } from "@prisma/client";
+import { DndContext } from "@dnd-kit/core";
 
 import PreviewDailogButton from "./preview-dailog-button";
 import SaveFormButton from "./save-form-button";
 import PublishFormButton from "./publish-form-button";
 import Designer from "./designer";
+import DragOverlayWrapper from "../partials/drag-overlay-wrapper";
 
 const FormBuilder = ({ form }: { form: Form }) => {
   return (
-    <main className="flex flex-col w-full">
-      <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
-        <h2 className="truncate font-medium">
-          <span className="text-muted-foreground mr-2">Form:</span>
-          {form.name}
-        </h2>
-        <div className="flex items-center gap-2">
-          <PublishFormButton />
-          {!form.published && (
-            <>
-              <SaveFormButton />
-              <PreviewDailogButton />
-            </>
-          )}
+    <DndContext>
+      <main className="flex flex-col w-full">
+        <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
+          <h2 className="truncate font-medium">
+            <span className="text-muted-foreground mr-2">Form:</span>
+            {form.name}
+          </h2>
+          <div className="flex items-center gap-2">
+            <PublishFormButton />
+            {!form.published && (
+              <>
+                <SaveFormButton />
+                <PreviewDailogButton />
+              </>
+            )}
+          </div>
+        </nav>
+        <div className="w-full flex flex-grow items-center justify-center relative overflow-y-auto h-[200px] bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]">
+          <Designer />
         </div>
-      </nav>
-      <div className="w-full flex flex-grow items-center justify-center relative overflow-y-auto h-[200px] bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]">
-        <Designer />
-      </div>
-    </main>
+      </main>
+      <DragOverlayWrapper />
+    </DndContext>
   );
 };
 
---
+++ components/layouts/sidebar-button-element-drag-overlay.tsx
@@ -0,0 +1,33 @@
+import { useDraggable } from "@dnd-kit/core";
+
+import { FormElement } from "../partials/form-elements";
+
+import { Button } from "../ui/button";
+
+function SidebarBtnElementDragOverlay({
+  formElement,
+}: {
+  formElement: FormElement;
+}) {
+  const { label, icon: Icon } = formElement.designerButtonElement;
+  const draggable = useDraggable({
+    id: `designer-btn-${formElement.type}`,
+    data: {
+      type: formElement.type,
+      isDesignerBtnElement: true,
+    },
+  });
+
+  return (
+    <Button
+      ref={draggable.setNodeRef}
+      variant={"outline"}
+      className="flex flex-col gap-2 h-[120px] w-[120px] cursor-grab"
+    >
+      <Icon className="h-8 w-8 text-primary cursor-grab" />
+      <p className="text-xs">{label}</p>
+    </Button>
+  );
+}
+
+export default SidebarBtnElementDragOverlay;
